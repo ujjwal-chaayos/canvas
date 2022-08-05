8f470320e84878e9b4b75319646c8bc85fe1941b
LAPTOP-MV16TJFV\ujjwa <ujjwal@chaayos.com>
2022-08-05 15:43:48 +0530

    add-routing

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,10 +1,14 @@
-import Test from './components/Test';
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import AllRoutes from './Routes'
+
 
 function App() {
   return (
     <div className="App">
-      <Test />
+      <Router>
+      <AllRoutes />
+      </Router>
     </div>
   );
 }
