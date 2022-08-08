16f0fa4674d9f461fc4a1cfc5dfbbfe012efdfa8
LAPTOP-2QAFV03K\ritvi <ritvik@chaayos.com>
2022-08-08 15:26:32 +0530

    staging changes.

--- sample/src/Services/CVServices.js
+++ sample/src/Services/CVServices.js
@@ -1,10 +1,20 @@
 import cv from "opencv.js";
 
+
+// function will return the coordinate of all blocks provided in template
 export default function getCoordinates(template, cv) {
+
+    // converting template into Gray Scale for processing.
     cv.cvtcolor(template, template, cv.RGBA2GRAY)
+
+    // Finding the Threshold image which which will define our contours.
     cv.threshold(template, template, 120, 200, cv.THRESH_BINARY);
+
+    // defining storage to store contours and hierarchy.
     let contours = new cv.ImageVector();
     let hierarchy = new cv.Image();
+
+    // finding contours in template
     cv.findContours(
         template,
         contours,
@@ -12,7 +22,10 @@ export default function getCoordinates(template, cv) {
         cv.RETR_CCOMP,
         cv.CHAIN_APPROX_SIMPLE
     );
-    const points = [];
+
+    // stroing coordinates of each blocks define by contour in format of [{x,y,w,h}] , (x,y) are the coordinate of top left corner of blocks
+    // and w,h are the height and width of respective block.
+    const coordinates = [];
     for (let i = 0; i < contours.size(); ++i) {
         const ci = contours.get(i);
         let rect = cv.boundingRect(ci);
@@ -21,29 +34,52 @@ export default function getCoordinates(template, cv) {
         p.y = rect.y;
         p.w = rect.width;
         p.h = rect.height;
-        points.push(p);
+        coordinates.push(p);
     }
-    return points;
+
+    // returning determined coordinates
+    return coordinates;
 
 }
+
+// This function will draw bounded reactangle with dashed line over the block on canvas(Screen).
 export default function drawContours(points, cv, screen) {
+
+    // this will set the width of line to 2.
     screen.lineWidth = "2";
+
+    // this will set thw width of dashes to 4.
     screen.setLineDash([4]);
+
+    // this will define the colour of bounded reactangle to blue.
     screen.strokeStyle = "blue";
+    
+    // this will draw the rectangle over the provided block on canvas.
     screen.rect(parseInt(points.x),parseInt(points.y),parseInt(points.w), parseInt(points.h));
     screen.stroke();
 }
 
+// This function will return the resized Image matrix.
 export default function reSize(image, width, height, cv) {
+
+    // definig storage to store resized image
     let finalImage = new cv.Mat();
+
+    // definig the size of final image.
     let finalImageSize = new cv.Size(width, height);
+
+    // check whether the image is shrinking or expanding.
     if ((width * height) < (image.size().width * image.size().height))
         cv.resize(image, finalImage, finalImageSize, 0, 0, cv.INTER_AREA);
     else
         cv.resize(image, finalImage, finalImageSize, 0, 0, cv.INTER_LINEAR);
+
+    // returning the final image.
     return finalImage
 }
 
+// This function will convert image matrix into image data.
+// and after converting to Imagedata return the imagedata.
 export default function matrixToImgData(image){
     var tempImg = new cv.Mat;
     var depth = image.type() % 8;
@@ -64,60 +100,89 @@ export default function matrixToImgData(image){
             return;
     }
     var imgData = new ImageData(new Uint8ClampedArray(tempImg.data), tempImg.cols, tempImg.rows);
+    tempImg.delete();
     return imgData;
 }
 
+// This function will put the image onto the canvas(screen).
 export default function drawImage(screen, image, points) {
     
+    // this will put the image at specified position.
     screen.putImageData(image,points['x'],points['y']);
-    tempImg.delete();
+
+    // save the screen.
     screen.save();
 }
 
+// This function will put Text on the screen.
 export default function drawText(screen,text,points,style){
-
+    // This will define the Font to text to be showwn.
+    screen.font = style;
+    // Putting the text on the screen.
+    screen.fillText(text,parseInt(points.x),parseInt(points.y));
 }
 
-
+// This function will draw Line below the Title.
 export default function drawLine(screen,sourcePoint,destinationPoint,style){
+
+    // defining the width of line equal to 20.
     screen.lineWidth = 20;
+    
+    // defining starting point of line.
     screen.moveTo(parseInt(sourcePoint.x), parseInt(sourcePoint.y));
+
+    // defining the ending point of line.
     screen.lineTo(parseInt(destinationPoint.x), parseInt(destinationPoint.y));
+
+    // defining the color of line and finally draw it then save it.
     screen.strokeStyle = "#AB8E3E";
     screen.stroke();
     screen.save();
 }
 
+// This will download the final screen as png.
 export default function download(screen)
 {
+    // getting the URL of image and return that URL.
     let imageURL = screen.toDataURL("image/png");
     return imageURL;
 }
 
+
+// This function will find the coordinates of sub-blocks within a given block.
 export default function subBlockCoordinates(points,headingHeight,priceWidth){
+    // to store final coordinates of sub-block.
     let finalCoordinates = {}
+    // to store the coordinates of heading. 
     let heading ={};
+    // to store the coordinates of priceList.
     let priceList = {};
+    // to store the coordinates of itemList.
     let itemList={};
 
+    // finding the coordinates of heading i.e x,y,w,h.
     heading.x = parseInt(points.x);
     heading.y = parseInt(points.y);
     heading.h = headingHeight;
     heading.w = parseInt(points.w);
 
+    // finding the coordinates of priceList i.e x,y,w,h.
     priceList.x = parseInt(points.x) + (parseInt(points.w)-priceWidth);
     priceList.y = parseInt(points.y) + headingHeight;
     priceList.h = parseInt(points.h) - headingHeight;
     priceList.w = priceWidth;
 
+    // finding the coordinates of itemList i.e x,y,w,h.
     itemList.x = parseInt(points.x);
     itemList.y = parseInt(points.y) + headingHeight;
     itemList.h = parseInt(points.h) - headingHeight;
     itemList.w = parseInt(points.w) - priceWidth;
 
+    // puting all sub-block's coordinates into finalcoordinate lsit.
     finalCoordinates.title = heading;
-    finalCoordinates.description = itemList;
+    finalCoordinates.items = itemList;
     finalCoordinates.price = priceList;
+    // returning the final coordinates list.
     return finalCoordinates;
 
 }
