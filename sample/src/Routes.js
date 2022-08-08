1ff6d240e72da8f4d0ee0fb6d5cb1629b3fa0da3
LAPTOP-PT2VU00E\isha <isha@chaayos.com>
2022-08-08 17:06:31 +0530

    Choose temp

--- sample/src/Routes.js
+++ sample/src/Routes.js
@@ -3,6 +3,8 @@ import {Routes,Route} from 'react-router-dom'
 import GenOrPick from './Pages/GenOrPick'
 import Screen from './Pages/Screen'
 import Home from './Pages/Home'
+import ChooseTemplate from './Pages/ChooseTemplate'
+import UploadTemplate from './Pages/UploadTemplate'
 
 
 const AllRoutes = () => {
@@ -11,6 +13,8 @@ const AllRoutes = () => {
           <Route path="/" element={<Home/>}/>
           <Route path="/screen" element={<Screen/>}/>
           <Route path="/pick" element={<GenOrPick/>}/>
+          <Route path="/select-template" element={<ChooseTemplate/>}/>
+          <Route path="/upload-template" element={<UploadTemplate/>}/>
       </Routes>
     )
   }
