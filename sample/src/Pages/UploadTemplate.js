349ed7c569e9bf63208fa5c7be99603614375897
LAPTOP-MV16TJFV\ujjwa <ujjwal@chaayos.com>
2022-08-09 13:25:03 +0530

    files-cleaned

--- sample/src/Pages/UploadTemplate.js
+++ sample/src/Pages/UploadTemplate.js
@@ -1,69 +1,74 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { Box, Button} from "@mui/material";
+import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Button } from "@mui/material";
 import "./UploadTemplate.css";
 //import data from '../data/';
 
 const UploadTemplate = () => {
-  
-  const [file,setFile]=useState([]);
+  const [file, setFile] = useState([]);
   let dummy_data;
 
   async function read(file) {
     // Read the file as text
     //console.log(await file.text())
     // Read the file as ArrayBuffer to handle binary data
-    console.log(new Uint8Array(await file.arrayBuffer()))
-    let finalData=new Uint8Array(await file.arrayBuffer());
+    //console.log(new Uint8Array(await file.arrayBuffer()));
+    let finalData = new Uint8Array(await file.arrayBuffer());
     return finalData;
-
   }
- 
 
-  const handleUpload=async (event)=>{
-    let imgInfo={'imageId':'','imageInfo':'','imageType':'','imageContent':'','imageBlob':''};
-    let data=[];
-    imgInfo['imageBlob']=URL.createObjectURL(event.target.files[0]);
+  const handleUpload = async (event) => {
+    let imgInfo = {
+      imageId: "",
+      imageInfo: "",
+      imageType: "",
+      imageContent: "",
+      imageBlob: "",
+    };
+    let data = [];
+    imgInfo["imageBlob"] = URL.createObjectURL(event.target.files[0]);
     for (let myfile of event.target.files) {
-      let comingdata=await read(myfile);
-        console.log(comingdata);
-        imgInfo['imageContent']=comingdata;
+      let comingdata = await read(myfile);
+      imgInfo["imageContent"] = comingdata;
     }
-    const files=event.target.files;
-    console.log(typeof(files[0]))
-    imgInfo['imageInfo']=files[0];
+    const files = event.target.files;
+    
+    imgInfo["imageInfo"] = files[0];
+    imgInfo["imageType"] = files[0].name.split(".")[1];
+    imgInfo["imageId"] = event.target.id;
+
+    //console.log(URL.createObjectURL(event.target.files[0]))
+    setFile([...file, [imgInfo]]);
+  };
   
-    imgInfo['imageType']=files[0].name.split(".")[1];
-    imgInfo['imageId']=event.target.id;
-    console.log(imgInfo);
 
-  //console.log(URL.createObjectURL(event.target.files[0]))
-  setFile([...file,[imgInfo]])
-  }
-  console.log(file);
-
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if(file.length===2){
-      dummy_data=file;   //condition to save data
-      console.log(dummy_data)
-    }
-    else{
+    if (file.length === 2) {
+      dummy_data = file; //condition to save data
+      console.log(dummy_data);
+    } else {
       alert("Insert both Images..");
     }
+  };
 
-    
-  }
- 
   return (
-
-
     <div>
-    <form onSubmit={handleSubmit}>
-      <input type="file" id="template"  name="file1" onChange={(e)=>handleUpload(e)} />
-      <input type="file" id="background"  name="file2" onChange={(e)=>handleUpload(e)} />
-      <button type="submit"> NEXT </button>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          id="template"
+          name="file1"
+          onChange={(e) => handleUpload(e)}
+        />
+        <input
+          type="file"
+          id="background"
+          name="file2"
+          onChange={(e) => handleUpload(e)}
+        />
+        <button type="submit"> NEXT </button>
+      </form>
     </div>
   );
 };
