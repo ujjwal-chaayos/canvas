b1e46a405adf4648d58315cfe5f5dc40b357a1dc
LAPTOP-MV16TJFV\ujjwa <ujjwal@chaayos.com>
2022-08-09 13:16:11 +0530

    image-upload-validation-done

--- sample/src/Pages/UploadTemplate.js
+++ sample/src/Pages/UploadTemplate.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { Box, Button} from "@mui/material";
 import "./UploadTemplate.css";
+//import data from '../data/';
 
 const UploadTemplate = () => {
   
   const [file,setFile]=useState([]);
-
+  let dummy_data;
 
   async function read(file) {
     // Read the file as text
@@ -14,12 +15,12 @@ const UploadTemplate = () => {
     // Read the file as ArrayBuffer to handle binary data
     console.log(new Uint8Array(await file.arrayBuffer()))
     let finalData=new Uint8Array(await file.arrayBuffer());
-    return finalData
+    return finalData;
 
   }
  
 
-  const handleSubmit=async (event)=>{
+  const handleUpload=async (event)=>{
     let imgInfo={'imageId':'','imageInfo':'','imageType':'','imageContent':'','imageBlob':''};
     let data=[];
     imgInfo['imageBlob']=URL.createObjectURL(event.target.files[0]);
@@ -35,19 +36,33 @@ const UploadTemplate = () => {
     imgInfo['imageType']=files[0].name.split(".")[1];
     imgInfo['imageId']=event.target.id;
     console.log(imgInfo);
+
   //console.log(URL.createObjectURL(event.target.files[0]))
-  setFile([...file,...data])
+  setFile([...file,[imgInfo]])
   }
   console.log(file);
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(file.length===2){
+      dummy_data=file;   //condition to save data
+      console.log(dummy_data)
+    }
+    else{
+      alert("Insert both Images..");
+    }
+
+    
+  }
  
   return (
 
 
     <div>
-    <form >
-      <input type="file" id="template"  name="file1" onChange={(e)=>handleSubmit(e)} />
-      <input type="file" id="background"  name="file2" onChange={(e)=>handleSubmit(e)} />
-      <button type="submit" className="btn btn-info"> Update File </button>
+    <form onSubmit={handleSubmit}>
+      <input type="file" id="template"  name="file1" onChange={(e)=>handleUpload(e)} />
+      <input type="file" id="background"  name="file2" onChange={(e)=>handleUpload(e)} />
+      <button type="submit"> NEXT </button>
     </form>
     </div>
   );
