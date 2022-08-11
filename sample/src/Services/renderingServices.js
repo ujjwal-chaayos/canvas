fcc695036edfd0f1b26d2acbe29dccc7da045914
LAPTOP-2QAFV03K\ritvi <ritvik@chaayos.com>
2022-08-11 17:13:32 +0530

    error resolve

--- sample/src/Services/renderingServices.js
+++ sample/src/Services/renderingServices.js
@@ -74,7 +74,7 @@ export async function mergeTemplateBackground(template, background) {
 }
 
 export async function drawProductImage(background,imageData,coordinateData){
-  console.log(background,imageData,coordinateData)
+  //console.log(background,imageData,coordinateData)
   let bgImg = new Image();
   bgImg.src = background;
   await loadImage(bgImg);
@@ -83,8 +83,38 @@ export async function drawProductImage(background,imageData,coordinateData){
     bgImg.height
   );
   let screen1ctx = screen1canvas.getContext("2d");
-  
+  for(var i=0;i<imageData.length; i++){
+    //console.log(imageData[i]);
+    let imgBlockId = imageData[i].block_id;
+    let itemImgInfo = imageData[i].image_info.imageBlob;
+    let productImg = new Image();
+    productImg.src = itemImgInfo;
+    await loadImage(productImg);
+    for(var j=0;j<coordinateData.length; j++){
+      let coordinateBlockId = coordinateData[j].block_id;
+      if(parseInt(imgBlockId)===parseInt(coordinateBlockId)){
+        let points = {};
+        points.x = coordinateData[j].x;
+        points.y = coordinateData[j].y;
+        points.w = coordinateData[j].w;
+        points.h = coordinateData[j].h;
+        console.log(points);
+        console.log(itemImgInfo);
+        console.log(typeof(productImg));
+        drawImage(screen1ctx,productImg,points);
+        screen1ctx.save();
 
+        
+      }
+    }
+  }
+
+  let blob = await screen1canvas.convertToBlob();
+  let arraybuffer = await blob.arrayBuffer();
+  var uint8View = new Uint8Array(arraybuffer);
+   blob = new Blob( [ uint8View ], { type: "image/png" } );
+   return {blob};
+  
 }
 
 
