cc2cd11070a80c0535e0d6a0d4e923bd26129524
LAPTOP-6R0A5O7C\kush <kush@chaayos.com>
2022-08-17 16:24:08 +0530

    staging changes.

--- sample/src/Services/JSONConverter.js
+++ sample/src/Services/JSONConverter.js
@@ -1,7 +1,111 @@
 import { subBlockCoordinates } from "./CVServices";
 import { sortCoordinates } from "./CVServices";
 
-export function jsonConverter(menu, refrenceTemplate,font) {
+export function uiJsonConverter(menu, mapping) {
+  let titles = {};
+  let items = {};
+  let prices = {};
+  var subCategory = menu["menuSequence"]["sub-category"];
+  var products = menu["productDetail"]["products"];
+  var productPrices = menu["prices"]["prices"];
+  for (let i = 0; i < mapping.length; i++) {
+    for (let j = 0; j < mapping.length; j++) {
+      if (
+        subCategory[j].name.toLowerCase() === mapping[i].value.toLowerCase()
+      ) {
+        var pids = subCategory[j].pids;
+        var id = mapping[i].block_id.toString();
+        titles[id] = {
+          value: mapping[i].value,
+          block_id: id,
+          title_id: subCategory[j].id,
+        };
+        
+        let item = [];
+        let value = [];
+        for (let k = 0; k < pids.length; k++) {
+          let id = pids[k].toString();
+          item.push({
+            item_id: id,
+            value: products[id].name,
+            icons: products[id].attr,
+            active: products[id].active,
+            new: products[id].tag === null ? false : true,
+          });
+          let values = [];
+          for (let h = 0; h < productPrices[id].prices.length; h++) {
+            let obj = productPrices[id].prices[h];
+            values.push({
+              id: obj.id,
+              price: obj.price,
+            });
+          }
+          value.push({
+            item_id: id,
+            value: values,
+          });
+        }
+        items[id] = {
+          block_id: id,
+          qty: item.length,
+          item: item,
+        };
+        // items.push({
+        //   id: {
+        //     block_id: id,
+        //     qty: item.length,
+        //     item: item,
+        //   },
+        // });
+        prices[id] = {
+          block_id: id,
+          qty: value.length,
+          value: value,
+        };
+        // prices.push({
+        //   id: {
+        //     block_id: id,
+        //     qty: value.length,
+        //     value: value,
+        //   },
+        // });
+      }
+    }
+  }
+  return {
+    titles: titles,
+    items: items,
+    prices: prices,
+    "style":{
+      font: {
+        Title: "Helvetica, Arial, sans-serif",
+        Items: "Helvetica, Arial, sans-serif",
+        Prices: "Helvetica, Arial, sans-serif",
+        New: "Helvetica, Arial, sans-serif",
+      },
+      size: {
+        Title: "96px",
+        Items: "56px",
+        Prices: "56px",
+        New: "60px",
+      },
+      color: {
+        Title: "#376902",
+        Items: "#827311",
+        Prices: "#000000",
+        New: "#827311",
+      },
+      weight: {
+        Title: "Bold",
+        Items: "",
+        Prices: "",
+        New: "Bold",
+      },
+    }
+  };
+}
+
+export function jsonConverter(menu, refrenceTemplate, font) {
   var imageBlocks = [];
   var textBlocks = [];
   for (let i = 0; i < refrenceTemplate["templates"].length; i++) {
@@ -36,7 +140,7 @@ export function jsonConverter(menu, refrenceTemplate,font) {
   var productPrices = menu["prices"]["prices"];
   var productImages = menu["productImages"];
 
- // var images = {};
+  // var images = {};
   //var titles = [];
   refinedTemplate["images"] = { qty: "" };
   refinedTemplate["titles"] = { qty: subCategory.length - 1 };
@@ -47,7 +151,7 @@ export function jsonConverter(menu, refrenceTemplate,font) {
     if (subCategory[i]["name"] === "Images") {
       refinedTemplate["images"]["qty"] = subCategory[i]["pids"].length;
       for (let j = 0; j < subCategory[i]["pids"].length; j++) {
-        refinedTemplate["images"][(imageBlocks[j]["block_id"]).toString()] = {
+        refinedTemplate["images"][imageBlocks[j]["block_id"].toString()] = {
           img_id: subCategory[i]["pids"][j],
           imgURL:
             productImages["baseUrl"] +
@@ -70,7 +174,7 @@ export function jsonConverter(menu, refrenceTemplate,font) {
           active: products[subCategory[i]["pids"][j].toString()]["active"],
           new: products[subCategory[i]["pids"][j].toString()]["new"],
         });
-       
+
         var prices =
           productPrices[subCategory[i]["pids"][j].toString()]["prices"];
         let values = [];
@@ -95,60 +199,60 @@ export function jsonConverter(menu, refrenceTemplate,font) {
         font
       );
 
-      refinedTemplate["items"][(textBlocks[c]["block_id"]).toString()]={
+      refinedTemplate["items"][textBlocks[c]["block_id"].toString()] = {
         block_id: textBlocks[c]["block_id"],
         template_id: textBlocks[c]["template_id"],
         qty: subCategory[i]["pids"].length,
         item: item,
       };
 
-      refinedTemplate["prices"][(textBlocks[c]["block_id"]).toString()]={
+      refinedTemplate["prices"][textBlocks[c]["block_id"].toString()] = {
         block_id: textBlocks[c]["block_id"],
         template_id: textBlocks[c]["template_id"],
         qty: subCategory[i]["pids"].length,
         value: value,
       };
 
-      refinedTemplate["titles"][(textBlocks[c]["block_id"]).toString()]={
+      refinedTemplate["titles"][textBlocks[c]["block_id"].toString()] = {
         title_id: subCategory[i]["id"],
         value: subCategory[i]["name"],
         block_id: textBlocks[c]["block_id"],
-        template_id: textBlocks[c]["template_id"]
+        template_id: textBlocks[c]["template_id"],
       };
-      textBlocks.splice(c,1);
+      textBlocks.splice(c, 1);
     }
   }
- // refinedTemplate["titles"]["title"] = titles;
+  // refinedTemplate["titles"]["title"] = titles;
 
   //default style from db
   //avilable icons from db
 
-  refinedTemplate["style"] ={
-    "font": {
-       "Title": "Helvetica, Arial, sans-serif",
-       "Items": "Helvetica, Arial, sans-serif",
-       "Prices": "Helvetica, Arial, sans-serif",
-       "New":"Helvetica, Arial, sans-serif"
+  refinedTemplate["style"] = {
+    font: {
+      Title: "Helvetica, Arial, sans-serif",
+      Items: "Helvetica, Arial, sans-serif",
+      Prices: "Helvetica, Arial, sans-serif",
+      New: "Helvetica, Arial, sans-serif",
     },
-    "size": {
-      "Title": "96px",
-      "Items": "56px",
-      "Prices": "56px",
-      "New":"60px"
-   },
-    "color":{
-      "Title": "#376902",
-      "Items": "#827311",
-      "Prices": "#000000",
-      "New":"#827311"
-   },
-    "weight": {
-      "Title": "Bold",
-      "Items": "",
-      "Prices": "",
-      "New":"Bold"
-   }
-} //default
+    size: {
+      Title: "96px",
+      Items: "56px",
+      Prices: "56px",
+      New: "60px",
+    },
+    color: {
+      Title: "#376902",
+      Items: "#827311",
+      Prices: "#000000",
+      New: "#827311",
+    },
+    weight: {
+      Title: "Bold",
+      Items: "",
+      Prices: "",
+      New: "Bold",
+    },
+  }; //default
 
   return refinedTemplate;
 }
@@ -191,30 +295,37 @@ export function getBestBlock(blocks, data, font) {
   screen.font = font.h2 + " " + font.style;
   let quantity = parseInt(data.qty);
   let textheight = parseInt(font.h2);
-  textheight = (textheight +  parseInt(font.spacing))*quantity;
+  textheight = (textheight + parseInt(font.spacing)) * quantity;
   let difference = Number.MAX_SAFE_INTEGER;
 
-  for(let i=0;i<blocks.length;i++) {
+  for (let i = 0; i < blocks.length; i++) {
     let priceWidth = 0.2 * parseInt(blocks[i].w);
     let width = parseInt(blocks[i].w) - priceWidth;
     let height = parseInt(blocks[i].h) - parseInt(font.h1);
-    let textArea = width*textheight;
-    let blockArea = width*height;
-    console.log("blockarea "+blockArea+" textarea "+textArea+" id "+blocks[i].block_id+" qty "+quantity)
+    let textArea = width * textheight;
+    let blockArea = width * height;
+    console.log(
+      "blockarea " +
+        blockArea +
+        " textarea " +
+        textArea +
+        " id " +
+        blocks[i].block_id +
+        " qty " +
+        quantity
+    );
     if (blockArea > textArea) {
       console.log("in if");
       if (difference > blockArea - textArea) {
         difference = blockArea - textArea;
         index = i;
-        console.log("index "+i);
+        console.log("index " + i);
       }
     }
   }
-  console.log("final index "+index);
+  console.log("final index " + index);
   return index;
-
 }
-
 
 export function createCoordinateJSON(
   templateID,
@@ -240,7 +351,7 @@ export function createCoordinateJSON(
     e["template_id"] = templateID;
     e["type"] = "Text";
     var subCoordinates = subBlockCoordinates(e, font.h1, e.w * 0.2);
-   
+
     var heading = {
       block_id: 1,
       parent_block_id: e["block_id"],
