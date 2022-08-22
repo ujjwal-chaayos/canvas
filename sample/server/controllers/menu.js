c20fd9bb73ef5e9570f12a9242570d2bdc67688e
LAPTOP-PT2VU00E\isha <isha@chaayos.com>
2022-08-22 17:25:02 +0530

    changes

--- sample/server/controllers/menu.js
+++ sample/server/controllers/menu.js
@@ -11,8 +11,8 @@ const {mergeTemplateBackground}= require('../services/mergeTemplateBackground')
 exports.uploadTemplate = (req, res) => {
         console.log("uploadTemplate Called")
         let data= req.body;
-     let src1 =     "data:image/png;base64,"+ btoa(String.fromCharCode.apply(null, [data['file1']['imageContent']]));
-     let src2 =     "data:image/png;base64,"+ btoa(String.fromCharCode.apply(null, [data['file2']['imageContent']]));
+        let src1 =     "data:image/png;base64,"+ btoa(String.fromCharCode.apply(null, [data['file1']['imageContent']]));
+        let src2 =     "data:image/png;base64,"+ btoa(String.fromCharCode.apply(null, [data['file2']['imageContent']]));
 
         mergeTemplateBackground(data['file1']['imageInfo'],data['file2']['imageInfo']);
         res.send("Hello World!");
