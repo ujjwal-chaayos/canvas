3faab0b6020c16c3dc4e06055c547965d4ea66a2
LAPTOP-2QAFV03K\ritvi <ritvik@chaayos.com>
2022-08-25 12:56:18 +0530

    saving

--- sample/client/src/Pages/5-UploadTemplate/UploadTemplate.js
+++ sample/client/src/Pages/5-UploadTemplate/UploadTemplate.js
@@ -101,26 +101,15 @@ const UploadTemplate = () => {
     let returnedImg ='data:image/jpeg;base64,' +response.data['backgroundWithContours'];
     localStorage.setItem("returnedImgWithContours",returnedImg);
     console.log(returnedImg);
-    //console.log(response.data['backgroundWithContours'],response.data['background'],response.data['sortedCoordinates']);
-    //   console.log(dummy_data);
-    //   console.log(file);
-    //   let { blob, blob2, sortedCoordinates } = await mergeTemplateBackground(
-    //     file[0][0]["imageBlob"],
-    //     file[1][0]["imageBlob"]
-    //   );
     
 
-       
-
-    //   console.log(blob);
-
-    //   let temp = document.getElementById("background1");
+      
 
     //   let link = URL.createObjectURL(blob);
     //   let link2 = URL.createObjectURL(blob2);
     //   console.log(link, link2);
     //   setOriginalImg(link2);
-    //   setResultImage(link);
+       setResultImage(returnedImg);
     //   setCoordinates(sortedCoordinates);
     } else {
       alert("Insert both Images..");
