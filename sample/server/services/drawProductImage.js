82f8d0f35cdc0256b7c77098fb47810b28b1c055
LAPTOP-6R0A5O7C\kush <kush@chaayos.com>
2022-08-29 11:22:07 +0530

    staging changes.

--- sample/server/services/drawProductImage.js
+++ sample/server/services/drawProductImage.js
@@ -24,7 +24,9 @@ const { uiJsonConverter } = require("./JSONConverter")
 // const vegIcon = require("../data/background/veg icon.svg")
 
 // const menu = require("../data/Menus/menu.json");
-const { createCanvas,Image } = require('canvas');
+const GIFEncoder = require('gifencoder');
+const { createCanvas } = require('canvas');
+const fs = require('fs');
 
 
 
@@ -33,7 +35,13 @@ const drawProductImage =  (background, imageData, coordinateData) => {
   bgImg.onload = ()=>{};
   bgImg.src = background.data;
  
-  
+  const encoder = new GIFEncoder(bgImg.width, bgImg.height);
+// stream the results as they are available into myanimated.gif
+encoder.createReadStream().pipe(fs.createWriteStream('myanimated.gif'));
+encoder.start();
+encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
+encoder.setDelay(500);  // frame delay in ms
+encoder.setQuality(10); // image quality. 10 is default.
   let screen1canvas = createCanvas(parseInt(bgImg.width), parseInt(bgImg.height));
   let screen1ctx = screen1canvas.getContext("2d");
  drawImage(screen1ctx, bgImg, {
@@ -42,10 +50,9 @@ const drawProductImage =  (background, imageData, coordinateData) => {
     w: bgImg.width,
     h: bgImg.height,
   });
-
+let coordinate=[];
   for (var i = 0; i < imageData.length; i++) {
     let imgBlockId = imageData[i].block_id.split('_')[0];
-    // let itemImgInfo = imageData[i].image_info.imageBlob;
     if (parseInt(imageData[i].block_id.split('_')[1]) === 0) {
       let itemImgInfo = imageData[i].imageInfo;
       let productImg = new Image();
@@ -59,15 +66,29 @@ const drawProductImage =  (background, imageData, coordinateData) => {
           points.y = coordinateData[j].y;
           points.w = coordinateData[j].w;
           points.h = coordinateData[j].h;
+          let arr = [];
+          for (var k = 0; k < imageData.length; k++) {
+            if( parseInt(imageData[k].block_id.split('_')[0])===parseInt(imgBlockId) ){
+              arr.push(imageData[k]);
+            }
+          }
+          coordinate.push({
+            coordinte:coordinateBlockId,
+            image:arr
+          });
           console.log(imgBlockId,coordinateBlockId);
           drawImage(screen1ctx, productImg, points);
           screen1ctx.save();
-
-
+          encoder.addFrame(screen1ctx);
         }
       }
     }
   }
+console.log(coordinate);
+  for (var i = 0; i < coordinate.length; i++) {
+
+  }
+
   let buffer1 = screen1canvas.toBuffer('image/png').toString('base64');
 
   return { "ImageWithProducts": buffer1 };
