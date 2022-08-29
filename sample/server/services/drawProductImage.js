b8433159d2b69d4541c973e6ca0b35fcbd01f213
LAPTOP-2QAFV03K\ritvi <ritvik@chaayos.com>
2022-08-29 13:35:43 +0530

    staging changes.

--- sample/server/services/drawProductImage.js
+++ sample/server/services/drawProductImage.js
@@ -25,29 +25,187 @@ const { uiJsonConverter } = require("./JSONConverter")
 
 // const menu = require("../data/Menus/menu.json");
 const { createCanvas,Image } = require('canvas');
+const GIFEncoder = require('gifencoder');
+
+const fs = require('fs');
+const { set } = require("mongoose");
 
 
+// const drawProductImage =  (background, imageData, coordinateData) => {
 
-const drawProductImage =  (background, imageData, coordinateData) => {
-  let bgImg = new Image();
-  bgImg.onload = ()=>{};
-  bgImg.src = background.data;
- 
+//   let imageArray = [];
   
-  let screen1canvas = createCanvas(parseInt(bgImg.width), parseInt(bgImg.height));
-  let screen1ctx = screen1canvas.getContext("2d");
- drawImage(screen1ctx, bgImg, {
-    x: 0,
-    y: 0,
-    w: bgImg.width,
-    h: bgImg.height,
-  });
 
+
+//   let bgImg = new Image();
+//   bgImg.onload = ()=>{};
+//   bgImg.src = background.data;
+ 
+//   const encoder = new GIFEncoder(bgImg.width, bgImg.height);
+// // stream the results as they are available into myanimated.gif
+// encoder.createReadStream().pipe(fs.createWriteStream('./data/myanimated.gif'));
+// encoder.start();
+// encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
+// encoder.setDelay(500);  // frame delay in ms
+// encoder.setQuality(10); // image quality. 10 is default.
+  
+//   let screen1canvas = createCanvas(parseInt(bgImg.width), parseInt(bgImg.height));
+//   let screen1ctx = screen1canvas.getContext("2d");
+//  drawImage(screen1ctx, bgImg, {
+//     x: 0,
+//     y: 0,
+//     w: bgImg.width,
+//     h: bgImg.height,
+//   });
+
+//   let h = 0;
+//   let coordinate=[];
+//   for (var i = 0; i < imageData.length; i++) {
+//     let imgBlockId = imageData[i].block_id.split('_')[0];
+//     if (parseInt(imageData[i].block_id.split('_')[1]) === 0) {
+//       let itemImgInfo = imageData[i].imageInfo;
+//       let productImg = new Image();
+//       productImg.onload = ()=>{};
+//       productImg.src = itemImgInfo;
+//       for (var j = 0; j < coordinateData.length; j++) {
+//         let coordinateBlockId = coordinateData[j].block_id;
+//         if (parseInt(imgBlockId) === parseInt(coordinateBlockId)) {
+//           let points = {};
+//           points.x = coordinateData[j].x;
+//           points.y = coordinateData[j].y;
+//           points.w = coordinateData[j].w;
+//           points.h = coordinateData[j].h;
+//           let arr = [];
+//           for (var k = 0; k < imageData.length; k++) {
+//             if( parseInt(imageData[k].block_id.split('_')[0])===parseInt(imgBlockId) ){
+//               let img = new Image();
+//               img.onload = ()=>{};
+//               img.src = imageData[k].imageInfo;
+//               arr.push(img);
+//             }
+//           }
+//           h=Math.max(h,arr.length);
+//           coordinate.push({
+//             pointData:points,
+//             image:arr,
+//             len:arr.length
+//           });
+//           console.log(imgBlockId,coordinateBlockId);
+//           drawImage(screen1ctx, productImg, points);
+//           screen1ctx.save();
+         
+//         }
+//       }
+//     }
+//   }
+//   encoder.addFrame(screen1ctx);
+//   for (var i = 0; i < h; i++) {
+//     for (var j = 0; j < coordinate.length; j++) {
+//       let len =  coordinate[j].len;
+//       drawImage(screen1ctx, coordinate[j].image[i%len], coordinate[j].pointData);
+//       screen1ctx.save();
+//     }
+//     encoder.addFrame(screen1ctx);
+//     console.log(i);
+//   }
+// console.log("finished");
+  
+
+//   // for (var i = 0; i < imageData.length; i++) {
+//   //   let imgBlockId = imageData[i].block_id.split('_')[0];
+//   //   // let itemImgInfo = imageData[i].image_info.imageBlob;
+//   //   if (parseInt(imageData[i].block_id.split('_')[1]) === 0) {
+//   //     let itemImgInfo = imageData[i].imageInfo;
+//   //     let productImg = new Image();
+//   //     productImg.onload = ()=>{};
+//   //     productImg.src = itemImgInfo;
+//   //     for (var j = 0; j < coordinateData.length; j++) {
+//   //       let coordinateBlockId = coordinateData[j].block_id;
+//   //       if (parseInt(imgBlockId) === parseInt(coordinateBlockId)) {
+//   //         let points = {};
+//   //         points.x = coordinateData[j].x;
+//   //         points.y = coordinateData[j].y;
+//   //         points.w = coordinateData[j].w;
+//   //         points.h = coordinateData[j].h;
+//   //         console.log(imgBlockId,coordinateBlockId);
+//   //         drawImage(screen1ctx, productImg, points);
+//   //         screen1ctx.save();
+
+
+//   //       }
+//   //     }
+//   //   }
+//   // }
+//   let buffer1 = screen1canvas.toBuffer('image/png').toString('base64');
+//   encoder.finish();
+//   return { "ImageWithProducts": buffer1 };
+
+// }
+
+
+const drawProductImage =  (background, imagedict, coordinateData) => {
+  let imageData = [];
+  let ids = new Set();
+  for(var i=0;i<imagedict.length;i++){
+    var imgBlockId = imagedict[i].block_id.split('_')[0];
+    if(!ids.has(imgBlockId)){
+      ids.add(imgBlockId);
+      var info = {};
+      info.block_id = imgBlockId;
+      info.imgInfo = [];
+      imageData.push(info);
+    }
+    
+  }
+
+  //console.log(imageData.length)
+  //console.log(imageData[0].block_id)
+  for(var i=0;i<imagedict.length;i++){
+    var imgBlockId = imagedict[i].block_id.split('_')[0];
+    for(var j=0;j<imageData.length;j++){
+      if(parseInt(imageData[j].block_id) === parseInt(imgBlockId) ){
+        imageData[j].imgInfo.push(imagedict[i].imageInfo)
+      }
+    }
+  }
+
+  let maxImages = 0;
   for (var i = 0; i < imageData.length; i++) {
-    let imgBlockId = imageData[i].block_id.split('_')[0];
-    // let itemImgInfo = imageData[i].image_info.imageBlob;
-    if (parseInt(imageData[i].block_id.split('_')[1]) === 0) {
-      let itemImgInfo = imageData[i].imageInfo;
+    console.log(imageData[i].imgInfo.length);
+    if (maxImages < imageData[i].imgInfo.length) {
+      maxImages = imageData[i].imgInfo.length;
+    }
+  }
+  console.log(maxImages);
+  let finalImagesArray = [];
+
+  for (var index = 0; index < maxImages; index++) {
+    let bgImg = new Image();
+    bgImg.onload = ()=>{};
+    bgImg.src = background.data;
+    let screen1canvas = createCanvas(
+      parseInt(bgImg.width),
+      parseInt(bgImg.height)
+    );
+    let screen1ctx = screen1canvas.getContext("2d");
+
+    drawImage(screen1ctx, bgImg, {
+      x: 0,
+      y: 0,
+      w: bgImg.width,
+      h: bgImg.height,
+    });
+    for (var i = 0; i < imageData.length; i++) {
+      let imgBlockId = imageData[i].block_id;
+      let reqiredImageIndex;
+      if (index >= imageData[i].imgInfo.length) {
+        reqiredImageIndex = imageData[i].imgInfo.length - ((index + 1) % imageData[i].imgInfo.length) - 1;
+      }
+      else {
+        reqiredImageIndex = index;
+      }
+      //let reqiredImageIndex = index % imageData[i].image_info.length;
+      let itemImgInfo = imageData[i].imgInfo[reqiredImageIndex];
       let productImg = new Image();
       productImg.onload = ()=>{};
       productImg.src = itemImgInfo;
@@ -59,20 +217,85 @@ const drawProductImage =  (background, imageData, coordinateData) => {
           points.y = coordinateData[j].y;
           points.w = coordinateData[j].w;
           points.h = coordinateData[j].h;
-          console.log(imgBlockId,coordinateBlockId);
           drawImage(screen1ctx, productImg, points);
           screen1ctx.save();
-
-
         }
       }
     }
+    let buffer = screen1canvas.toBuffer('image/png').toString('base64');
+    finalImagesArray.push(buffer);
   }
-  let buffer1 = screen1canvas.toBuffer('image/png').toString('base64');
+  //console.log(finalImagesArray.length);
+  //console.log(finalImagesArray[0]);
+  return finalImagesArray;
 
-  return { "ImageWithProducts": buffer1 };
+
 
 }
 
-
 module.exports = drawProductImage;
+
+
+
+
+
+// export async function CreateGifImage(background, imageData, coordinateData) {
+
+//   let maxImages = 0;
+//   for (var i = 0; i < imageData.length; i++) {
+//     if (maxImages < imageData[i].imgInfo.length) {
+//       maxImages = imageData[i].imgInfo.length;
+//     }
+//   }
+//   let finalImagesArray = [];
+//   for (var index = 0; index < maxImages; index++) {
+//     let bgImg = new Image();
+//     bgImg.src = background;
+//     await loadImage(bgImg);
+//     let screen1canvas = new OffscreenCanvas(
+//       bgImg.width,
+//       bgImg.height
+//     );
+//     let screen1ctx = screen1canvas.getContext("2d");
+
+//     drawImage(screen1ctx, bgImg, {
+//       x: 0,
+//       y: 0,
+//       w: bgImg.width,
+//       h: bgImg.height,
+//     });
+//     for (var i = 0; i < imageData.length; i++) {
+//       let imgBlockId = imageData[i].block_id;
+//       let reqiredImageIndex;
+//       if (index >= imageData[i].image_info.length) {
+//         reqiredImageIndex = imageData[i].image_info.length - ((index + 1) % imageData[i].image_info.length) - 1;
+//       }
+//       else {
+//         reqiredImageIndex = index;
+//       }
+//       //let reqiredImageIndex = index % imageData[i].image_info.length;
+//       let itemImgInfo = imageData[i].image_info[reqiredImageIndex].blob;
+//       let productImg = new Image();
+//       productImg.src = itemImgInfo;
+//       await loadImage(productImg);
+//       for (var j = 0; j < coordinateData.length; j++) {
+//         let coordinateBlockId = coordinateData[j].block_id;
+//         if (parseInt(imgBlockId) === parseInt(coordinateBlockId)) {
+//           let points = {};
+//           points.x = coordinateData[j].x;
+//           points.y = coordinateData[j].y;
+//           points.w = coordinateData[j].w;
+//           points.h = coordinateData[j].h;
+//           drawImage(screen1ctx, productImg, points);
+//           screen1ctx.save();
+//         }
+//       }
+//     }
+//     let blob = await screen1canvas.convertToBlob();
+//     let arraybuffer = await blob.arrayBuffer();
+//     var uint8View = new Uint8Array(arraybuffer);
+//     blob = new Blob([uint8View], { type: "image/png" });
+//     finalImagesArray.push(blob);
+//   }
+//   return finalImagesArray;
+// }
