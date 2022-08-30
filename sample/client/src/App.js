aa103dcae983dd20798b94e8aa89013b7fd35f50
LAPTOP-MV16TJFV\ujjwa <ujjwal.1923co1003@kiet.edu>
2022-08-30 10:50:36 +0530

    main-branch-back

--- sample/client/src/App.js
+++ sample/client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import AllRoutes from "./routes/Routes";
 // import { CSSTransition } from "react-transition-group";
 
+//main branch is back
+
 import "./main.css";
 
 function App() {
