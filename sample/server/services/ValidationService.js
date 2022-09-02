1506bcf1e2b66859e2e32a16919220c7e17f5f01
LAPTOP-2QAFV03K\ritvi <ritvik@chaayos.com>
2022-09-02 13:23:34 +0530

    wrap done

--- sample/server/services/ValidationService.js
+++ sample/server/services/ValidationService.js
@@ -26,19 +26,48 @@ const widthValidation = (block, txt, font) => {
   }
   return txtWidth < block.w ? true : false;
 };
+// const wrapValidation = (block, txt, font) => {
+//   var txtHeight;
+//   var txtWidth;
+//   let screen = new createCanvas(3840, 2160).getContext("2d");
+//   if (Array.isArray(txt)) {
+//     txtHeight = (font.height + font.spacing) * txt.length;
+//   }
+//   for (let i = 0; i < txt.length; i++) {
+//     txtWidth = Math.max(txtWidth, Math.floor(screen.measureText(txt[i]).width));
+//   }
+//   if (txtHeight / 2 < block.height && txtWidth * 2 < block.width * 0.2)
+//     return true;
+//   else return false;
+// };
+
+
 const wrapValidation = (block, txt, font) => {
-  var txtHeight;
-  var txtWidth;
+  console.log("i m in validation");
+  var txtWidth = 0;
   let screen = new createCanvas(3840, 2160).getContext("2d");
-  if (Array.isArray(txt)) {
-    txtHeight = (font.height + font.spacing) * txt.length;
-  }
+  var blockWidth =  Math.ceil((block.w*100)/80) - 0.4*Math.ceil((block.w*100)/80);
+  var blockHeight = block.h;
+  var txtHeight = font.height + font.spacing;
+  var totatNumberitem = Math.floor(blockHeight/txtHeight);
+
+  console.log("hi" ,totatNumberitem,blockHeight,txtHeight)
   for (let i = 0; i < txt.length; i++) {
-    txtWidth = Math.max(txtWidth, Math.floor(screen.measureText(txt[i]).width));
+    txtWidth = Math.max(txtWidth, Math.floor(screen.measureText(txt[i].value).width));
   }
-  if (txtHeight / 2 < block.height && txtWidth * 2 < block.width * 0.2)
+  console.log(txt.length,totatNumberitem+1,txtWidth*2,blockWidth);
+
+  if((txt.length > totatNumberitem+1)  && ((txtWidth*2)<=blockWidth)){
+    console.log("i m true");
     return true;
-  else return false;
+  }
+  else {
+    console.log("i am false");
+    return false
+  };
+
+
+  
 };
 
 module.exports = { heightValidation, widthValidation, wrapValidation };
