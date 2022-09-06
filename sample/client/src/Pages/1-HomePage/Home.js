bed70939a9c161992b708c6c3903d7661af4e72a
LAPTOP-MV16TJFV\ujjwa <ujjwal@chaayos.com>
2022-09-06 14:42:07 +0530

    button-add

--- sample/client/src/Pages/1-HomePage/Home.js
+++ sample/client/src/Pages/1-HomePage/Home.js
@@ -9,11 +9,20 @@ import {
   FormControl,
   Chip,
   Typography,
+  Button
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import "./Home.css";
 
+
+
+
 const Home = () => {
+
+
+  const login = (e) => {
+      e.preventDefault();
+  }
   return (
     <div>
       <Box
@@ -29,55 +38,67 @@ const Home = () => {
         }}
       >
         <div class="help">
-            <Box sx={{ border: 1, py:5,px:15, borderRadius: 3, borderColor: 'primary.main' }}>
+          <Box
+            sx={{
+              border: 1,
+              py: 5,
+              px: 15,
+              borderRadius: 3,
+              borderColor: "primary.main",
+            }}
+          >
             <Typography variant="h4" component="h2">
-                WELCOME TO{" "}
-                <Typography
-                  variant="h2"
-                  component="h2"
-                  sx={{ color: "#0f0f0f" }}
-                >
-                  CLIPPY
-                </Typography>
+              WELCOME TO{" "}
+              <Typography variant="h2" component="h2" sx={{ color: "#0f0f0f" }}>
+                CLIPPY
               </Typography>
-              <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div
-      style={{
-        "justifyContent": "center",
-        "backgroundColor": "primary.light",
-        "display": "grid",
-      }}
-       >
-        <TextField
-          required
-          id="outlined-required"
-          label="User Id"
-          placeholder="User Id"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-      </div>
-      </Box>
+            </Typography>
+            <form oneSubmit={login}>
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "25ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <div
+                style={{
+                  justifyContent: "center",
+                  backgroundColor: "primary.light",
+                  display: "grid",
+                
+                }}
+              >
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="User Id"
+                  placeholder="User Id"
+                />
+                <TextField
+                  id="outlined-password-input"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                  sx={{py:1}}
+                />
+              
+              <Button type="submit" variant="contained" size="large" >Login</Button>
+
+           
+              </div>
             </Box>
 
-            <Link
-              to={`/screen`}
-              style={{ "text-decoration": "none", color: "#FFF" }}
-            >
-             
-            </Link>
-            
+               
+            </form>
+ 
+          </Box>
+
+          <Link
+            to={`/screen`}
+            style={{ "text-decoration": "none", color: "#FFF" }}
+          ></Link>
         </div>
       </Box>
     </div>
